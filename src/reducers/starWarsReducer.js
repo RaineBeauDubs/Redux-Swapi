b3b9a16198a806bbb21c2d53a2496e7192c9fb84
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-06 15:55:10 -0500

    I believe I've set up my actions and my reducer correctly, but I'm still not able to see anything on the screen because I havn't gotten to that error yet, so I can't be sure.

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,13 +1,25 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_START, FETCH_SUCCESS, FETCH_ERROR } from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  error: ''
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
+    case FETCH_START:
+      return {
+        ...state
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        characters: action.payload
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      }
     default:
       return state;
   }
